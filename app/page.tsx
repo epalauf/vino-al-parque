e2fa87a031ca0119fa78167b7ec44a0c43ddfd2f
epalauf <epalau@gmail.com>
2023-09-12 06:52:48 -0500

    google maps added

--- app/page.tsx
+++ app/page.tsx
@@ -60,22 +60,21 @@ export default function Home() {
 				<div className="page-section__gallery__col">
 					<p>
 						Una dedicada selección de vinos con más de 700 referencias para que disfrutes la mejor variedad de vinos en Colombia, y lo mejor, por copeo. 
-						Más de 14 países presentes
-						Pet friendly
-						A cielo abierto en el museo parque el chicó
-						Aforo limitado
-						Evento para mayores de edad											
+																	
 					</p>
 				</div>
 				<div className="page-section__gallery__col">
 					<CarouselComponent />
 				</div>
 			</div>
+			<div className="page-section page-section__map" >
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.5518383859776!2d-74.04676212517019!3d4.673684241891491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f99740ee43781%3A0xd9d997c44051f15e!2sMuseo%20del%20Chic%C3%B3!5e0!3m2!1ses!2sco!4v1694519132328!5m2!1ses!2sco" width="100%" height="450" style={{border: 0}}  loading="lazy" allowFullScreen={true} referrerPolicy="no-referrer"></iframe>
+			</div>
 
 			<div className="page-section page-section__contact" >
 				<ContactComponent />	
 				<div className="page-section page-section__contact__text">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nunc lacus, fermentum vitae ultricies vitae, aliquet in ligula. Nullam iaculis ipsum eget metus aliquam, vel fringilla diam cursus. Phasellus posuere diam sed enim tincidunt, nec pharetra arcu consectetur.</p>
+					<p>Más de 14 países presentes Pet friendly A cielo abierto en el museo parque el chicó Aforo limitado Evento para mayores de edad.</p>
 				
 				</div>		
 			</div>
