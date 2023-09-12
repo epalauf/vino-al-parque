58a6a5e6ad6c2d49926a9e228e70d7d3ec519b2d
epalauf <epalau@gmail.com>
2023-09-12 10:20:17 -0500

    responsive styles

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "Vino al Parque",
+	description: "Edicion 2023",
 	navItems: [
 		{
 			label: "Home",
